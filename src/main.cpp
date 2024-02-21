6f326639c10633330230e3c2d8c481326c659403
smart-life-tech <christlightworld@gmail.com>
2024-02-21 04:51:31 +0100

    modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,16 @@
 #include <InfluxDbCloud.h>
 #include <UMS3.h>
 #include <esp_bt.h>
+#include <SPIFFS.h>
+#include <Adafruit_NeoPixel.h>
+#include <FS.h>
+
+#define PIN 2      // Input your RGB LED module GPIO pin
+#define NUM_LEDS 1 // Number of LEDs in your module
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
+
+int DELAYVAL = 500; // Time (in milliseconds) to pause between pixels
 
 UMS3 ums3;
 
@@ -35,15 +45,110 @@ InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKE
 Point sensorReadings("measurements");
 
 #define ONE_WIRE_BUS 1
+#define DATA_FILE "/data.txt"
+// Global variables to track time and battery voltage
+unsigned long startMillis = 0;
+float initialBatteryVoltage = 0.0;
+int fileCount = 0;
+struct DataPoint
+{
+  float temperature;
+  float batteryVoltage;
+};
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
-void initDS18B20() {
+// Input a value 0 to 255 to get a color value.
+uint32_t Wheel(byte WheelPos)
+{
+  WheelPos = 255 - WheelPos;
+  if (WheelPos < 85)
+  {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if (WheelPos < 170)
+  {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+void rainbow(uint8_t wait)
+{
+  uint16_t i, j;
+
+  for (j = 0; j < 256 * 2; j++)
+  { // repeat twice for a smoother transition
+    for (i = 0; i < strip.numPixels(); i++)
+    {
+      strip.setPixelColor(i, Wheel((i + j) & 255));
+    }
+    strip.show();
+    delay(wait); // we acnt dekay here any more we will use the general 30 secs delay
+    // break;
+  }
+}
+void saveDataToSPIFFS(float temperature, float batteryVoltage)
+{
+  File file = SPIFFS.open(DATA_FILE, FILE_APPEND);
+  if (!file)
+  {
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+
+  DataPoint dataPoint = {temperature, batteryVoltage};
+
+  file.write((uint8_t *)&dataPoint, sizeof(DataPoint));
+  file.close();
+  Serial.println("data saved to spiffs");
+}
+void uploadDataFromSPIFFS()
+{
+  File file = SPIFFS.open(DATA_FILE, FILE_READ);
+  if (!file)
+  {
+    Serial.println("No data to upload from SPIFFS");
+    return;
+  }
+
+  while (file.available())
+  {
+    DataPoint dataPoint;
+    size_t bytesRead = file.read((uint8_t *)&dataPoint, sizeof(DataPoint));
+
+    if (bytesRead == sizeof(DataPoint))
+    {
+      sensorReadings.clearFields();
+      sensorReadings.addField("temperature", dataPoint.temperature);
+      sensorReadings.addField("battery_voltage", dataPoint.batteryVoltage);
+
+      Serial.print("Writing data from SPIFFS: ");
+      Serial.println(client.pointToLineProtocol(sensorReadings));
+
+      client.writePoint(sensorReadings);
+    }
+    else
+    {
+      Serial.println("Error reading data from SPIFFS");
+    }
+    delay(1000);
+  }
+
+  file.close();
+
+  // Delete data file from SPIFFS after upload
+  SPIFFS.remove(DATA_FILE);
+}
+void initDS18B20()
+{
   sensors.begin();
 }
-
-void setup() {
+int trial = 0;
+void setup()
+{
   Serial.begin(115200);
   ums3.begin();
   sensors.begin();
@@ -54,9 +159,17 @@ void setup() {
   wifiMulti.addAP(WIFI_SSID2, WIFI_PASSWORD2);
   wifiMulti.addAP(WIFI_SSID3, WIFI_PASSWORD3);
   btStop();
-  while (wifiMulti.run() != WL_CONNECTED) {
+
+  while (wifiMulti.run() != WL_CONNECTED)
+  {
     Serial.print(".");
     delay(500);
+    trial++;
+    if (trial > 10)
+    {
+      trial = 0;
+      break;
+    }
   }
   Serial.println();
 
@@ -67,38 +180,138 @@ void setup() {
 
   timeSync(TZ_INFO, "pool.ntp.org", "time.nis.gov");
 
-  if (client.validateConnection()) {
+  if (client.validateConnection())
+  {
     Serial.print("Connected to InfluxDB: ");
     Serial.println(client.getServerUrl());
-  } else {
+  }
+  else
+  {
     Serial.print("InfluxDB connection failed: ");
     Serial.println(client.getLastErrorMessage());
   }
+
+  // Initialize SPIFFS
+  if (!SPIFFS.begin())
+  {
+    Serial.println("Failed to mount file system");
+    return;
+  }
+  SPIFFS.remove(DATA_FILE);
+  startMillis = millis();                           // Store the current time
+  initialBatteryVoltage = ums3.getBatteryVoltage(); // Store initial battery voltage
+  strip.begin();                                    // Initialize the NeoPixel library
+  strip.show();                                     // Initialize all pixels to 'off'
 }
 
-void loop() {
-  sensors.requestTemperatures();
+void loop()
+{
+  // Check USB presence
+  int usbPresence = ums3.getVbusPresent();
 
+  if (usbPresence == 0 && WiFi.status() != WL_CONNECTED)
+  {
+    WiFi.mode(WIFI_STA);
+    WiFi.reconnect();
+    wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
+    wifiMulti.addAP(WIFI_SSID1, WIFI_PASSWORD1);
+    wifiMulti.addAP(WIFI_SSID2, WIFI_PASSWORD2);
+    wifiMulti.addAP(WIFI_SSID3, WIFI_PASSWORD3);
+    btStop();
+    while (wifiMulti.run() != WL_CONNECTED)
+    {
+      Serial.print(".");
+      delay(500);
+      trial++;
+      if (trial > 10)
+      {
+        trial = 0;
+        break;
+      }
+    }
+    Serial.println("wifi connected");
+  }
+  sensors.requestTemperatures();
   float temperature = sensors.getTempCByIndex(0);
   float batteryVoltage = ums3.getBatteryVoltage();
   delay(1000);
-  int usbpresence = ums3.getVbusPresent();
-
   sensorReadings.addField("temperature", temperature);
   sensorReadings.addField("battery_voltage", batteryVoltage);
-  sensorReadings.addField("usb_presence", usbpresence);
-
-  Serial.print("Writing: ");
-  Serial.println(client.pointToLineProtocol(sensorReadings));
-
-  client.writePoint(sensorReadings);
-
-  sensorReadings.clearFields();
-
-  if (wifiMulti.run() != WL_CONNECTED) {
-    Serial.println("Wifi connection lost");
+  sensorReadings.addField("usb_presence", usbPresence);
+  // Upload data from SPIFFS if WiFi connection is restored
+  if (client.validateConnection())
+  {
+    Serial.print("Connected to InfluxDB: ");
+    Serial.println(client.getServerUrl());
+  }
+  else
+  {
+    Serial.print("InfluxDB connection failed: ");
+    Serial.println(client.getLastErrorMessage());
   }
 
+  // client.writePoint(sensorReadings);
+
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println("Wifi connection lost");
+    //  Save data to SPIFFS if USB power is not present
+    saveDataToSPIFFS(temperature, batteryVoltage);
+  }
+  else
+  {
+    // Upload data from SPIFFS if WiFi connection is restored
+    if (client.validateConnection())
+    {
+      Serial.print("Connected to InfluxDB: ");
+      Serial.println(client.getServerUrl());
+    }
+    else
+    {
+      Serial.print("InfluxDB connection failed: ");
+      Serial.println(client.getLastErrorMessage());
+    }
+    // Upload data from SPIFFS if WiFi connection is restored
+    uploadDataFromSPIFFS();
+    Serial.print("Writing: ");
+    Serial.println(client.pointToLineProtocol(sensorReadings));
+    client.writePoint(sensorReadings);
+    Serial.println(client.writePoint(sensorReadings));
+    sensorReadings.clearFields();
+  }
+  // Read battery voltage
+  float currentBatteryVoltage = ums3.getBatteryVoltage();
+
+  // Check if battery voltage is below 3.60V or 4 hours have elapsed (whichever comes first)
+  if ((currentBatteryVoltage < 3.60) || (millis() - startMillis >= 4 * 60 * 60 * 1000 && !ums3.getVbusPresent()))
+  {
+    // Shut down the ESP32
+    // esp_deep_sleep_start();
+    Serial.println("esp 32 will turn off now");
+  }
+  // Send data to InfluxDB if USB power is present
+  if (ums3.getVbusPresent() == 0)
+  {
+    if (fileCount == 0)
+    {
+      fileCount++;
+      rainbow(60 * 1000 / 256); // 60 seconds divided by 256-color spectrum
+      strip.clear();
+      strip.show();
+      if (fileCount >= 60)
+        fileCount = 0; // do thid every 60 seconds
+    }
+  }
+  else
+  {
+    fileCount = 0;
+    WiFi.disconnect();
+    WiFi.mode(WIFI_OFF);
+    Serial.println("wifi disconnected successfully");
+    // wifiMulti.disconnect();
+    //  Save data to SPIFFS if USB power is not present
+    saveDataToSPIFFS(temperature, batteryVoltage);
+  }
   Serial.println("Wait 30s");
   delay(30000);
 }
